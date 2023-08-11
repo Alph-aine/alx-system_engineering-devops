b7922b0d7f1851a7c2605fb3380e7f88bd67e90e
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-11 17:41:49 +0100

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fix 500 error when requesting from an Apache web server
 exec {'replace':
-  provider => shell,
   command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   }
