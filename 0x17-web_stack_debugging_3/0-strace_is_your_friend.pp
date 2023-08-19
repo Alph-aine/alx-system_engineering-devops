9712c4bb546366b2b60a55901f518afab8b7fb9a
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-19 10:26:05 +0100

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Fix 500 error when requesting from an Apache web server
 
-exec {'replace':
-  provider => shell,
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-  }
+exec {'config file':
+path     => ['/usr/bin', '/bin'],
+command  => "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+provider => 'shell',
+}
