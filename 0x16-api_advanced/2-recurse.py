376b32e07ebb2396b7069e3ac37479d122746c68
Abdulbasit <aabdulrafiu721@gmail.com>
2023-08-26 11:01:11 +0100

    fixed error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
-""" Get subreddit's articles recursively"""
+"""Get all hot articels of a subreddit"""
 from requests import get
-
 
 REDDIT = 'https://www.reddit.com/'
 HEADERS = {'User-Agent': 'my-app/0.0.1'}
 
 
-def recurse(subreddit, hot_list=[], after=''):
-    '''
-    Returns a list containing all hot articles of a sunreddit.
-    Returns None  if no result is found
-    '''
-    url = REDDIT + f'r/{subreddit}/hot.json/'
+def recurse(subreddit, hot_list=None, after=''):
+    """
+    Returns a list containing all hot article titles of a subreddit.
+    Returns None if no results are found.
+    """
+    if hot_list is None:
+        hot_list = []
+
+    if after is None:
+        return hot_list
+
+    url = REDDIT + f'r/{subreddit}/hot/.json/'
 
     params = {
-            'limit': 100,
-            'after': after,
-            }
+        'limit': 100,
+        'after': after,
+    }
 
-    request = get(url, headers=HEADERS, params=params, allow_redirects=False)
-    if r.status_code != 200:
+    response = get(url, headers=HEADERS, params=params, allow_redirects=False)
+    if response.status_code != 200:
         return None
 
     try:
-        response = request.json()
-    except ValueError:
-        return None
-
-    try:
-        data = response.get('data')
+        data = response.json().get('data')
         after = data.get('after')
         children = data.get('children')
+
         for child in children:
             post = child.get('data')
             hot_list.append(post.get('title'))
 
-    except (KeyError, TypeError, AttributeError):
+    except (KeyError, AttributeError, TypeError):
         return None
 
-    if after is None:
-        return hot_list
-    return (recurse(subreddit, hot_list, after))
+    return recurse(subreddit, hot_list, after)
